99bce07a173e506e950385d07c2cce8db3a7fad4
agapovaelyne <agapova.lina@gmail.com>
2021-12-07 00:09:12 +0300

    Add liquibase, remove docker-compose based db initialization

---
+++ src/main/resources/db/changelog/migrations/002-data.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset Elyne:001
+--validCheckSum: 8:bf219c22ac02e7c1daec889b03f835b7
+
+insert into users (login, password)
+values ('Elyne@gmail.com', '$2a$09$lGLoHmbdAusmMX/BnRV6defnPQN937UaEL//l0Rld5XvDrwe3kx/W'),
+       ('User1@mail.ru', '$2a$10$T2/QpCZf8kp5Xq1aMb.hyuC0G9Dr7/PzHqEE5/BYY2PCGA83Oq9.W');
+
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
+
+insert into user_roles (user_id, role_id)
+values (1, 2),
+       (2, 1);
--- target/classes/create_tables.sql
+++
@@ -1,5 +0,0 @@
-create table cloud_db.users
-(
-    id        serial primary key,
-    login        varchar(255)
-);
